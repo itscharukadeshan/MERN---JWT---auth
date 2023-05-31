e689b141f3b2810bc851d3b24b9a011e853a9119
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 20:40:35 +0530

    fix typo

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,11 +10,13 @@ const authUser = asyncHandler(async (req, res) => {
 const regUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExist = await User.findOne({ email });
-  if (userExist) {
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
     res.status(400);
-    throw new Error(" User already exist ");
+    throw new Error("User already exists");
   }
+
   const user = await User.create({
     name,
     email,
@@ -23,6 +25,7 @@ const regUser = asyncHandler(async (req, res) => {
 
   if (user) {
     genToken(res, user._id);
+
     res.status(201).json({
       _id: user._id,
       name: user.name,
