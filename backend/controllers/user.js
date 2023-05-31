53010f15e00da44faf7c2bfe6b9924619f5cfc2b
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 19:54:52 +0530

    add user register controller (without password)

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,7 +7,29 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ massage: "auth user" });
 });
 const regUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ massage: "register user" });
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error(" User already exist ");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data check the name , email and password");
+  }
 });
 const logOut = asyncHandler(async (req, res) => {
   res.status(200).json({ massage: "user logged out" });
