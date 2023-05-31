faec61021a066b82aeb9450c57c085edc05b8bad
charuka deshan <itscharukadeshan@gmail.com>
2023-05-31 20:02:25 +0530

    add password with encrypt to the user Schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -20,6 +21,14 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
