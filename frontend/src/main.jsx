6d4d3bd7ecc43e957439abb796bdbb9ca96b3a17
charuka deshan <itscharukadeshan@gmail.com>
2023-06-01 16:44:29 +0530

    add redux and store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,8 @@ import "./index.css";
 import HomeScreen from "./screesn/HomeScreen";
 import LoginScreen from "./screesn/LoginScreen";
 import RegisterScreen from "./screesn/RegisterScreen";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
---
+++ frontend/src/store.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: true,
+});
+
+export default store;
