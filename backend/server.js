355641a0342cc6f7b5d01571886ab1c358f7775f
charuka deshan <itscharukadeshan@gmail.com>
2023-06-02 20:47:27 +0530

    update file path and change the location of dist

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/backend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "backend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
